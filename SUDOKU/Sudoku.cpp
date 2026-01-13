d068c34710aad8e10348cc157f73561a9271e5d9
Daniel Dinev <azsumbg@gmail.com>
2026-01-13 19:23:37 +0200

    Add project files.

---
+++ SUDOKU/Sudoku.cpp
@@ -0,0 +1,229 @@
+#include "pch.h"
+#include "sudoku.h"
+
+// RANDIT *******************************
+
+dll::RANDIT::RANDIT()
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(),rd(),rd(),rd(),rd(),rd(),rd(),rd() };
+
+	twister = new std::mt19937(sq);
+}
+
+dll::RANDIT::~RANDIT()
+{
+	if (twister)delete twister;
+}
+
+int dll::RANDIT::operator()(int min, int max)
+{
+	std::uniform_int_distribution gen(min, max);
+
+	return gen(*twister);
+}
+
+float dll::RANDIT::operator()(float min, float max)
+{
+	std::uniform_real_distribution<float> gen(min, max);
+
+	return gen(*twister);
+}
+
+/////////////////////////////////////////
+
+// PROTON ******************************
+
+dll::PROTON::PROTON()
+{
+	_width = 1.0f;
+	_height = 1.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + _width / 2.0f;
+	center.y = start.y + _height / 2.0f;
+}
+dll::PROTON::PROTON(float _sx, float _sy) :start{ _sx, _sy }
+{
+	_width = 1.0f;
+	_height = 1.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + _width / 2.0f;
+	center.y = start.y + _height / 2.0f;
+}
+dll::PROTON::PROTON(float _sx, float _sy, float _s_width, float _s_height) :start{ _sx,_sy }, _width{ _s_width },
+_height{ _s_height }
+{
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + _width / 2.0f;
+	center.y = start.y + _height / 2.0f;
+}
+
+void dll::PROTON::Release()
+{
+	if (_in_heap)delete this;
+}
+
+void dll::PROTON::set_edges()
+{
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + _width / 2.0f;
+	center.y = start.y + _height / 2.0f;
+}
+void dll::PROTON::new_dims(float new_width, float new_height)
+{
+	_width = new_width;
+	_height = new_height;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + _width / 2.0f;
+	center.y = start.y + _height / 2.0f;
+}
+void dll::PROTON::new_width(float new_width)
+{
+	_width = new_width;
+	
+	end.x = start.x + _width;
+
+	center.x = start.x + _width / 2.0f;
+}
+void dll::PROTON::new_height(float new_height)
+{
+	_height = new_height;
+
+	end.y = start.y + _height;
+
+	center.y = start.y + _height / 2.0f;
+}
+
+float dll::PROTON::get_width()const
+{
+	return _width;
+}
+float dll::PROTON::get_height()const
+{
+	return _height;
+}
+
+bool dll::PROTON::in_heap()const
+{
+	return _in_heap;
+}
+
+dll::PROTON* dll::PROTON::create(float sx, float sy, float s_width, float s_height)
+{
+	PROTON* ret = new PROTON(sx, sy, s_width, s_height);
+	ret->_in_heap = true;
+	return ret;
+}
+
+////////////////////////////////////////
+
+// GRID *******************************
+
+dll::GRID::GRID()
+{
+	float next_x = 0;
+	float next_y = sky;
+
+	for (int rows = 0; rows < MAX_ROWS; ++rows)
+	{
+		for (int cols = 0; cols < MAX_COLS; ++cols)
+		{
+			int quadrant_row = 0;
+			int quadrant_col = 0;
+
+			if (rows < 3)quadrant_row = 0;
+			else if (rows < 6)quadrant_row = 1;
+			else quadrant_row = 2;
+
+			if (cols < 3)quadrant_col = 0;
+			else if (cols < 6)quadrant_col = 1;
+			else quadrant_col = 2;
+
+
+			game_grid[rows][cols].col = cols;
+			game_grid[rows][cols].row = rows;
+
+			game_grid[rows][cols].quadrant = quadrant_row + quadrant_col;
+
+			game_grid[rows][cols].dims.start.x = next_x;
+			game_grid[rows][cols].dims.start.y = next_y;
+			game_grid[rows][cols].dims.new_dims(50.0f, 50.0f);
+
+			next_x += 49.0f;
+		}
+
+		next_y += 49.0f;
+	}
+}
+
+int dll::GRID::get_value(int row, int col)const
+{
+	return(game_grid[row][col].value);
+}
+int dll::GRID::get_quadrant(int row, int col)const
+{
+	return game_grid[row][col].quadrant;
+}
+
+void dll::GRID::set_value(int row, int col, int new_value)
+{
+	int my_quadrant = game_grid[row][col].quadrant;
+	
+	game_grid[row][col].value = new_value;
+	game_grid[row][col].valid_number = true;
+
+	for (int rows = 0; rows < MAX_ROWS; ++rows)
+	{
+		for (int cols = 0; cols < MAX_ROWS; ++rows)
+		{
+			if (rows == row && cols == col)continue;
+
+			if (game_grid[rows][cols].quadrant == my_quadrant && game_grid[rows][cols].value == new_value)
+			{
+				game_grid[row][col].valid_number = false;
+				break;
+			}
+		}
+	}
+
+	for (int rows = 0; rows < MAX_ROWS; ++rows)
+	{
+		if (rows == row)continue;
+
+		if (game_grid[rows][col].value == new_value)
+		{
+			game_grid[row][col].valid_number = false;
+			break;
+		}
+	}
+
+	for (int cols = 0; cols < MAX_ROWS; ++cols)
+	{
+		if (cols == col)continue;
+
+		if (game_grid[row][cols].value == new_value)
+		{
+			game_grid[row][col].valid_number = false;
+			break;
+		}
+	}
+}
+bool dll::GRID::value_ok(int row, int col)const
+{
+	return game_grid[row][col].valid_number;
+}
+
+////////////////////////////////////////
---
+++ SUDOKU/sudoku.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#ifdef SUDOKU_EXPORTS
+#define SUDOKU_API __declspec(dllexport)
+#else
+#define SUDOKU_API __declspec(dllimport)
+#endif
+
+#include <random>
+
+constexpr int MAX_ROWS{ 9 };
+constexpr int MAX_COLS{ 9 };
+
+constexpr float scr_width{ 460.0f };
+constexpr float scr_height{ 560.0f };
+
+constexpr float sky{ 50.0f };
+constexpr float ground{ 510.0f };
+
+struct SUDOKU_API FPOINT
+{
+	float x{ 0 };
+	float y{ 0 };
+};
+struct SUDOKU_API FRECT
+{
+	float left{ 0 };
+	float up{ 0 };
+	float right{ 0 };
+	float down{ 0 };
+};
+
+
+namespace dll
+{
+	class SUDOKU_API RANDIT
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIT();
+
+		~RANDIT();
+
+		int operator()(int min, int max);
+
+		float operator()(float min, float max);
+	};
+	
+	class SUDOKU_API PROTON
+	{
+	private:
+		float _width{ 0 };
+		float _height{ 0 };
+
+		bool _in_heap = false;
+
+	public:
+		FPOINT start{};
+		FPOINT end{};
+		FPOINT center{};
+
+		PROTON();
+		PROTON(float _sx, float _sy);
+		PROTON(float _sx, float _sy, float _s_width, float _s_height);
+
+		virtual ~PROTON() {};
+
+		void Release();
+
+		void set_edges();
+		void new_dims(float new_width, float new_height);
+		void new_width(float new_width);
+		void new_height(float new_height);
+
+		float get_width()const;
+		float get_height()const;
+
+		bool in_heap()const;
+
+		static PROTON* create(float sx, float sy, float s_width, float s_height);
+	};
+
+	struct SUDOKU_API TILE
+	{
+		PROTON dims{};
+		int quadrant = -1;
+		int value = -1;
+		int row = -1;
+		int col = -1;
+		bool valid_number = false;
+	};
+
+	class SUDOKU_API GRID
+	{
+	private:
+		TILE game_grid[MAX_ROWS][MAX_COLS]{};
+
+	public:
+		GRID();
+
+		int get_value(int row, int col)const;
+		int get_quadrant(int row, int col)const;
+
+		void set_value(int row, int col, int new_value);
+		bool value_ok(int row, int col)const;
+	};
+}
